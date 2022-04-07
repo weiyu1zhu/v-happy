e3158e097f0c8bc0dd2d6e99bc2f011b826e4a77
Weiyu Zhu <weiyu.zhu@Weiyus-MacBook-Pro.local>
2022-04-07 16:47:41 -0700

    Modify video player and use in every tab component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,85 @@
 import VideoPlayer from './components/VideoPlayer';
 
 import './App.css';
-import Button from '@mui/material/Button';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import DataAdapter from '@mui/lab/AdapterMoment';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import React from 'react';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
 function App() {
+
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
   return (
     <LocalizationProvider dateAdapter={DataAdapter}>
       <div className="App">
         <header className="App-header">
           Welcome Tony! This is your V-Happy!
         </header>
-        <VideoPlayer />
+
+        <Box sx={{width: '100%'}}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
+            <Tabs value={tabValue} onChange={handleChange} aria-label="four major components">
+              <Tab label="Status" {...a11yProps(0)} style={{width: '25%'}}></Tab>
+              <Tab label="Historical" {...a11yProps(1)} style={{width: '25%'}}></Tab>
+              <Tab label="Video" {...a11yProps(2)} style={{width: '25%'}}></Tab>
+              <Tab label="Profiles" {...a11yProps(3)} style={{width: '25%'}}></Tab>
+            </Tabs>
+          </Box>
+          <TabPanel value={tabValue} index={0}>
+            <VideoPlayer />
+          </TabPanel>
+          <TabPanel value={tabValue} index={1}>
+            <VideoPlayer />
+          </TabPanel>
+          <TabPanel value={tabValue} index={2}>
+            <VideoPlayer />
+          </TabPanel>
+          <TabPanel value={tabValue} index={3}>
+            <VideoPlayer />
+          </TabPanel>
+        </Box>
       </div>
     </LocalizationProvider>
   );
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 import { Cameras, HOST, USER_ID, USER_TOKEN } from "../constants";
 import Video from "./Video";
 import DateTimePicker from '@mui/lab/DateTimePicker';
@@ -6,24 +6,20 @@ import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 
 import styles from "./VideoPlayer.module.css";
 
 const ONE_MINUTE_SECONDS = 60 * 1000;
 
 function VideoPlayer() {
-  const { start, end } = useMemo(
-    () => ({
-      start: Math.floor((Date.now() - 5 * ONE_MINUTE_SECONDS) / 1000),
-      end: Math.floor((Date.now() - 4 * ONE_MINUTE_SECONDS) / 1000),
-    }),
-    []
-  );
-
+  const [start, setStart] = useState(Math.floor((Date.now() - 5 * ONE_MINUTE_SECONDS) / 1000));
+  const [end, setEnd] = useState(Math.floor((Date.now() - 4 * ONE_MINUTE_SECONDS) / 1000));
+  
   const [videoMode, setVideoMode] = useState("live");
 
-  const [value, setDateTime] = useState(new Date());
+  const [date, setDateTime] = useState(new Date());
+  const [interval, setInterval] = useState(1);
 
   const [cameraId, setCameraId] = useState(Cameras[0].id);
   const src =
@@ -38,8 +34,8 @@ function VideoPlayer() {
   return (
     <div>
       <h3>Video Player</h3>
-      <div>
-        <FormControl fullWidth>
+      <div style={{marginBottom: '20px'}}>
+        <FormControl>
           <InputLabel id="camera-select-label"></InputLabel>
           <Select
             labelId="camera-select-label"
@@ -47,6 +43,7 @@ function VideoPlayer() {
             value={cameraId}
             label="Camera"
             onChange={(e) => setCameraId(e.target.value)}
+            style={{marginRight: '20px'}}
           >
             {Cameras.map((camera) => (
               <MenuItem value={camera.id}>
@@ -55,7 +52,7 @@ function VideoPlayer() {
             ))}
           </Select>
         </FormControl>
-        <FormControl fullWidth>
+        <FormControl>
           <InputLabel id="video-mode-select-label"></InputLabel>
           <Select
             labelId="video-mode-select-label"
@@ -63,22 +60,46 @@ function VideoPlayer() {
             value={videoMode}
             label="Video Mode"
             onChange={(e) => setVideoMode(e.target.value)}
+            style={{marginRight: '20px'}}
           >
             <MenuItem value="live">Live</MenuItem>
             <MenuItem value="historical">Historical</MenuItem>
           </Select>
         </FormControl>
-        <div>
-            <DateTimePicker
-              renderInput={(props) => <TextField {...props} />}
-              label="DateTimePicker"
-              value={value}
-              onChange={(newValue) => {
-                  setDateTime(newValue);
-                  console.log(newValue);
+        {videoMode === 'historical' ?
+        <div style={{marginTop: '20px'}}>
+          <DateTimePicker
+            renderInput={(props) => <TextField {...props} />}
+            label="Start time"
+            value={date}
+            onChange={(newDate) => {
+              setDateTime(newDate.toDate());
+              const newStart = newDate.toDate().getTime() / 1000;
+              setStart(newStart);
+              setEnd(newStart + interval * 60);
+            }}
+          />
+          <FormControl>
+            <InputLabel id="video-mode-select-label"></InputLabel>
+            <Select
+              labelId="interval-select-label"
+              id="interval-select-label"
+              value={interval}
+              label="Interval"
+              onChange={(e) => {
+                setInterval(e.target.value);
+                setStart(start);
+                setEnd(start + e.target.value * 60);
               }}
-            />
-        </div>
+              style={{marginLeft: '20px'}}
+            >
+              <MenuItem value="1">1 min</MenuItem>
+              <MenuItem value="2">2 min</MenuItem>
+              <MenuItem value="5">5 min</MenuItem>
+              <MenuItem value="10">10 min</MenuItem>
+            </Select>
+          </FormControl>
+        </div> : ''}
       </div>
       <Video className={styles.video} ref={onVideoElement} src={src} />
     </div>
