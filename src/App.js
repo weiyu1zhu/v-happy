005cd27e49093a12f27735789a82c44ee8702255
Feng Jiang <feng.jiang@verkada.com>
2022-04-07 21:32:46 -0700

    add user change

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import React from 'react';
 import {User} from "./utils/Classes";
+import {Tony} from "./constants";
 
 function TabPanel(props) {
     const {children, value, index, ...other} = props;
@@ -50,86 +51,93 @@ TabPanel.propTypes = {
 };
 
 class App extends React.Component {
-     constructor() {
+    constructor() {
         super();
         this.state = {
             tabValue: 0,
             anchorEl: null,
-            currentUser: 'Guest',
+            currentUser: 'Tony',
             users: {
-                "Tony": User.Default("Tony"),
+                "Tony": Tony,
                 "Guest": User.Default("Guest")
             }
         };
+
+        this.updateUsersProfile = this.updateUsersProfile.bind(this)
     }
 
+    updateUsersProfile(users) {
+        console.log(users)
+    }
 
-  render() {
-    return <LocalizationProvider dateAdapter={DataAdapter}>
-        <div className="App">
-          <div className="App-topper">
-            <header className="greeting-header">Welcome {this.state.currentUser}! This is your V-Happy!</header>
-            <IconButton aria-label="settings-button" size="large"
-                        aria-controls={Boolean(this.state.anchorEl) ? 'user-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={Boolean(this.state.anchorEl) ? 'true' : undefined}
-                        onClick={(event) => {
-                          this.setState({anchorEl: event.currentTarget});
-                        }}
-                        style={{position: 'absolute', bottom: '50px', right: '50px'}}>
-              <BrightnessHighIcon fontSize="large" style={{color: 'white'}}></BrightnessHighIcon>
-            </IconButton>
-            <Menu id="user-menu"
-                  anchorEl={this.state.anchorEl}
-                  open={Boolean(this.state.anchorEl)}
-                  onClose={() => {this.setState({anchorEl: null});}}
-                  MenuListProps={{
-                    'aria-labelledby': 'settings-button'
-                  }}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  transformOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right'
-                  }}>
-              {Object.entries(this.state.users).map(([userName, _]) => (
-                <MenuItem onClick={() => {
-                  this.setState({anchorEl: null});
-                  this.setState({currentUser: userName});
-                }}>{userName}</MenuItem>
-              ))}
-            </Menu>
-          </div>
+    render() {
+        return <LocalizationProvider dateAdapter={DataAdapter}>
+            <div className="App">
+                <div className="App-topper">
+                    <header className="greeting-header">Welcome {this.state.currentUser}! This is your V-Happy!</header>
+                    <IconButton aria-label="settings-button" size="large"
+                                aria-controls={Boolean(this.state.anchorEl) ? 'user-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={Boolean(this.state.anchorEl) ? 'true' : undefined}
+                                onClick={(event) => {
+                                    this.setState({anchorEl: event.currentTarget});
+                                }}
+                                style={{position: 'absolute', bottom: '50px', right: '50px'}}>
+                        <BrightnessHighIcon fontSize="large" style={{color: 'white'}}></BrightnessHighIcon>
+                    </IconButton>
+                    <Menu id="user-menu"
+                          anchorEl={this.state.anchorEl}
+                          open={Boolean(this.state.anchorEl)}
+                          onClose={() => {
+                              this.setState({anchorEl: null});
+                          }}
+                          MenuListProps={{
+                              'aria-labelledby': 'settings-button'
+                          }}
+                          anchorOrigin={{
+                              vertical: 'top',
+                              horizontal: 'right'
+                          }}
+                          transformOrigin={{
+                              vertical: 'bottom',
+                              horizontal: 'right'
+                          }}>
+                        {Object.entries(this.state.users).map(([userName, _]) => (
+                            <MenuItem onClick={() => {
+                                this.setState({anchorEl: null});
+                                this.setState({currentUser: userName});
+                            }}>{userName}</MenuItem>
+                        ))}
+                    </Menu>
+                </div>
 
-          <Box sx={{width: '100%'}}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
-              <Tabs value={this.state.tabValue} onChange={(event, newValue) => {
+                <Box sx={{width: '100%'}}>
+                    <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                        <Tabs value={this.state.tabValue} onChange={(event, newValue) => {
                             this.setState({tabValue: newValue})
                         }} aria-label="four major components">
-                <Tab label="Status" {...a11yProps(0)} style={{width: '25%'}}></Tab>
-                <Tab label="Historical" {...a11yProps(1)} style={{width: '25%'}}></Tab>
-                <Tab label="Video" {...a11yProps(2)} style={{width: '25%'}}></Tab>
-                <Tab label="Profiles" {...a11yProps(3)} style={{width: '25%'}}></Tab>
-              </Tabs>
-            </Box>
-            <TabPanel value={this.state.tabValue} index={0}>
-              <VideoPlayer />
-            </TabPanel>
-            <TabPanel value={this.state.tabValue} index={1}>
-              <VideoPlayer />
-            </TabPanel>
-            <TabPanel value={this.state.tabValue} index={2}>
-              <VideoPlayer />
-            </TabPanel>
-            <TabPanel value={this.state.tabValue} index={3}>
-              <Profiles />
-            </TabPanel>
-          </Box>
-        </div>
-      </LocalizationProvider>
-  }
+                            <Tab label="Status" {...a11yProps(0)} style={{width: '25%'}}></Tab>
+                            <Tab label="Historical" {...a11yProps(1)} style={{width: '25%'}}></Tab>
+                            <Tab label="Video" {...a11yProps(2)} style={{width: '25%'}}></Tab>
+                            <Tab label="Profiles" {...a11yProps(3)} style={{width: '25%'}}></Tab>
+                        </Tabs>
+                    </Box>
+                    <TabPanel value={this.state.tabValue} index={0}>
+                        <VideoPlayer/>
+                    </TabPanel>
+                    <TabPanel value={this.state.tabValue} index={1}>
+                        <VideoPlayer/>
+                    </TabPanel>
+                    <TabPanel value={this.state.tabValue} index={2}>
+                        <VideoPlayer/>
+                    </TabPanel>
+                    <TabPanel value={this.state.tabValue} index={3}>
+                        <Profiles currentUser={this.state.users[this.state.currentUser]} users={this.state.users} action={this.updateUsersProfile}/>
+                    </TabPanel>
+                </Box>
+            </div>
+        </LocalizationProvider>
+    }
 }
 
 export default App;
--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -5,75 +5,91 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import React, {Component} from "react";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 import {User} from "../utils/Classes";
 
-function Profiles() {
-    const defaultUser = User.Default("default")
-    return (
-        <div>
-            <h3>Profiles</h3>
+class Profiles extends Component {
+    // handleSubmit() {
+    //     let updatedUsers = {}
+    //     Object.assign(updatedUsers, this.props.users)
+    //     updatedUsers["yes"] = User.Default("yes")
+    //     console.log(updatedUsers)
+    //     this.props.action(updatedUsers)
+    // }
+
+    render() {
+        return (
             <div>
-                <TableContainer component={Paper}>
-                    <Table sx={{minWidth: 700}} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell align="right">Default Threshold</StyledTableCell>
-                                <StyledTableCell align="right">Temperature&nbsp;(F)</StyledTableCell>
-                                <StyledTableCell align="right">Air Quality Index&nbsp;(AQI)</StyledTableCell>
-                                <StyledTableCell align="right">Noise&nbsp;(db)</StyledTableCell>
-                                <StyledTableCell align="right">Humidity&nbsp;(%)</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            <StyledTableRow key="low yellow">
-                                <StyledTableCell align="left">Low Yellow</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.temperature.lowRed}</StyledTableCell>
-                                <StyledTableCell align="right">N/A</StyledTableCell>
-                                <StyledTableCell align="right">N/A</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.humidity.lowRed}</StyledTableCell>
-                            </StyledTableRow>
-                        </TableBody>
-                        <TableBody>
-                            <StyledTableRow key="low red">
-                                <StyledTableCell align="left">Low Red</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.temperature.lowYellow}</StyledTableCell>
-                                <StyledTableCell align="right">N/A</StyledTableCell>
-                                <StyledTableCell align="right">N/A</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.humidity.lowYellow}</StyledTableCell>
-                            </StyledTableRow>
-                        </TableBody>
-                        <TableBody>
-                            <StyledTableRow key="high yellow">
-                                <StyledTableCell align="left">High Yellow</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.temperature.highYellow}</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.airQuality.yellow}</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.noise.yellow}</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.humidity.highYellow}</StyledTableCell>
-                            </StyledTableRow>
-                        </TableBody>
-                        <TableBody>
-                            <StyledTableRow key="high red">
-                                <StyledTableCell align="left">High Red</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.temperature.highRed}</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.airQuality.red}</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.noise.red}</StyledTableCell>
-                                <StyledTableCell align="right">{defaultUser.humidity.highRed}</StyledTableCell>
-                            </StyledTableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                <h3>Profiles</h3>
+                <FormControl>
+                    <InputLabel id="user-select-label"/>
+                </FormControl>
+                <div>
+                    <TableContainer component={Paper}>
+                        <Table sx={{minWidth: 700}} aria-label="customized table">
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell align="right">{this.props.currentUser.name}'s Thresholds</StyledTableCell>
+                                    <StyledTableCell align="right">Temperature&nbsp;(F)</StyledTableCell>
+                                    <StyledTableCell align="right">Air Quality Index&nbsp;(AQI)</StyledTableCell>
+                                    <StyledTableCell align="right">Noise&nbsp;(db)</StyledTableCell>
+                                    <StyledTableCell align="right">Humidity&nbsp;(%)</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <StyledTableRow key="low red">
+                                    <StyledTableCell align="left">Low Red</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.temperature.lowRed}</StyledTableCell>
+                                    <StyledTableCell align="right">N/A</StyledTableCell>
+                                    <StyledTableCell align="right">N/A</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.humidity.lowRed}</StyledTableCell>
+                                </StyledTableRow>
+                            </TableBody>
+                            <TableBody>
+                                <StyledTableRow key="low yellow">
+                                    <StyledTableCell align="left">Low Yellow</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.temperature.lowYellow}</StyledTableCell>
+                                    <StyledTableCell align="right">N/A</StyledTableCell>
+                                    <StyledTableCell align="right">N/A</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.humidity.lowYellow}</StyledTableCell>
+                                </StyledTableRow>
+                            </TableBody>
+                            <TableBody>
+                                <StyledTableRow key="high yellow">
+                                    <StyledTableCell align="left">High Yellow</StyledTableCell>
+                                    <StyledTableCell
+                                        align="right">{this.props.currentUser.temperature.highYellow}</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.airQuality.yellow}</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.noise.yellow}</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.humidity.highYellow}</StyledTableCell>
+                                </StyledTableRow>
+                            </TableBody>
+                            <TableBody>
+                                <StyledTableRow key="high red">
+                                    <StyledTableCell align="left">High Red</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.temperature.highRed}</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.airQuality.red}</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.noise.red}</StyledTableCell>
+                                    <StyledTableCell align="right">{this.props.currentUser.humidity.highRed}</StyledTableCell>
+                                </StyledTableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+
+
+                {/*<form>*/}
+                {/*    <label>*/}
+                {/*        Name:*/}
+                {/*        <input type="text" name="name"/>*/}
+                {/*    </label>*/}
+                {/*    <input type="submit" value="Submit" onClick={this.props.action(this.props.users)}/>*/}
+                {/*</form>*/}
             </div>
-
-
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name"/>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
-        </div>
-    );
+        )
+    }
 }
 
 export default Profiles;
